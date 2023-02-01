ee327ef0491848d40ae4f1dee0f88ce4dc36fdaa
James Roberts <JRoberts86@gmail.com>
2023-02-01 23:34:33 +0000

    Publish files

--- train_nanoGPT.py
+++ train_nanoGPT.py
@@ -5,15 +5,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # Hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-device = torch.device('mps') if torch.backends.mps.is_available() and torch.backends.mps.is_built() else 'cpu'
+learning_rate = 1e-4
+# device = 'cuda' if torch.cuda.is_available() else 'cpu' # Nvidia CUDA
+device = torch.device('mps') if torch.backends.mps.is_available() and torch.backends.mps.is_built() else 'cpu' # Apple Silicon
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 with open('data/tinyshakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -105,6 +108,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B,T,C = x.shape
         k = self.key(x)   # (B,T,C)
@@ -113,6 +118,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         v = self.value(x) # (B,T,C)
         out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
@@ -124,9 +130,13 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
 
 class FeedFoward(nn.Module):
     """ A simple linear layer followed by a non-linearity """
@@ -134,8 +144,10 @@ class FeedFoward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
         )
 
     def forward(self, x):
@@ -150,11 +162,32 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
+    # With skip connection (x = x +)
     def forward(self, x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
+
+class LayerNorm1d: # (Used to be BatchNorm1d, switching rows to columns)
+  
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+
+    def __call__(self, x):
+        # calculate the forward pass
+        xmean = x.mean(1, keepdim=True) # batch mean
+        xvar = x.var(1, keepdim=True) # batch variance
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
 
 # Generative Pre-trained Transformer (GPT) - Single | Multi | FeedForward
 class GPTLanguageModel(nn.Module):
@@ -164,9 +197,8 @@ class GPTLanguageModel(nn.Module):
         # Each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        # self.sa_head = Head(n_embd) # Self-attention
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedFoward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -176,9 +208,8 @@ class GPTLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # B,T,C | C = nembd
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # T, C
         x = tok_emb + pos_emb # B,T,C
-        # x = self.sa_head(x) # Apply one head of self-attention (B,T,C)
-        x = self.sa_heads(x) # Apply multihead of self-attention (B,T,C)
-        x = self.ffwd(x)
+        x = self.blocks(x) # B,T,C
+        x = self.ln_f(x) # B,T,C
         logits = self.lm_head(x) # B,T,vocab_size
 
         if targets is None:
@@ -208,9 +239,9 @@ class GPTLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = GPTLanguageModel()
+model = BigramLanguageModel()
 m = model.to(device)
-print(sum(p.numel() for p in m.parameters()), 'parameters')
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 # Create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
